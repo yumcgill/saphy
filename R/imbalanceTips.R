97bce678c3fd38375f4d40023eec01905a995b2c
Yu Luo <yu.t.luo@mail.mcgill.ca>
2016-06-23 16:56:34 -0700

    update

--- R/imbalanceTips.R
+++ R/imbalanceTips.R
@@ -7,6 +7,8 @@
 #' @return a vector of tips name that causes the imbalance
 #' @author Yu Luo (\email{yu.luo3@mail.mcgill.ca})
 #' @export
+
+
 imbalanceTips<-function(tree,reps=100){
   treemetrics<-imbalanceMetrics(tree)
   thrg<-threthold(tree,reps=100)
@@ -14,13 +16,17 @@ imbalanceTips<-function(tree,reps=100){
                   |as.vector(unlist(treemetrics[1:12]))>thrg[1,]))>3)
   {
     ImbalanceTips<-NULL
+    seqtrees<-timeprune(tree)$trees[names(timeprune(tree)$trees)[3:tree$Nnode]]
+    thr<-lapply(seqtrees,function(x){threthold(x,reps)})
+    obs<-lapply(seqtrees,function(x){unlist(imbalanceMetrics(x)[1:12])})
     for(i in 3:(tree$Nnode)){
-      thr<-threthold(timeprune(tree)$trees[[i]],reps=100)
-      if(length(which(as.vector(unlist(imbalanceMetrics(timeprune(tree)$trees[[i]]))[1:12])<thr[2,]|
-                                as.vector(unlist(imbalanceMetrics(timeprune(tree)$trees[[i]])))[1:12]>thr[1,]))>3)
+      if(length(which(as.vector(unlist(imbalanceMetrics(timeprune(tree)$trees[[i]]))[1:12])<thr[[i]][2,|
+                                as.vector(unlist(imbalanceMetrics(timeprune(tree)$trees[[i]])))[1:12]>thr[[i]][1,]))>3)
       {
         ImbalanceTips<-c(ImbalanceTips, names(timeprune(tree)$trees)[i])
+
       }
     }
-    return(ImbalanceTips)}
-  else (return("This tree is balanced"))}
+    return(list(ImbalanceTips,thr,obs))
+   }
+  else (return("This tree is balanced",thr,obs))}
