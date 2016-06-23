a926f53d6bc11d174a40481f48162127dbae398c
Yu Luo <yu.t.luo@mail.mcgill.ca>
2016-06-23 11:36:17 -0700

    aa

---
+++ R/bootThreshold.R
@@ -0,0 +1,16 @@
+###return bootstrap threshold of the metric###
+
+threthold<-function(tree,reps){
+  thre<-NULL;bstrees<-permuteTrees(tree)
+  bslist<-lapply(bstrees,imbalanceMetrics)
+  treemetrics<-imbalanceMetrics(tree)
+  for (j in 1:12){
+    ql<-NULL;qu<-NULL;bs<-NULL
+    for (k in 1:reps){
+      metr<-bslist[[k]]
+      bs<-unlist(c(bs,metr[j]))}
+    ql<-c(ql,quantile(bs,prob=c(0.025)));qu<-c(qu,quantile(bs,prob=c(0.975)))
+    thre<-cbind(thre,rbind(qu,ql))
+
+  }
+  return(thre)}
